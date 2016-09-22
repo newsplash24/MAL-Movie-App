691447ac0cd8255abba11d1aa4ad27c5ea40e0af
newsplash24 <mohamed.hamed201328@gmail.com>
2016-09-22 22:47:19 +0200

    Update MainActivity.java

--- app/src/main/java/com/MhamedMalgp/moviesapp/MainActivity.java
+++ app/src/main/java/com/MhamedMalgp/moviesapp/MainActivity.java
@@ -3,13 +3,20 @@ package com.MhamedMalgp.moviesapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements MainActivityFragment.Callback,
         DetailActivityFragment.RecreateLoader {
 
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
     static Boolean mTwoPane;
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
+    private MenuItem item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,16 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
             mTwoPane = false;
             getSupportActionBar().setElevation(0f);
         }
+    }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (mTwoPane) {
+            getMenuInflater().inflate(R.menu.menu_double, menu);
+            item = menu.findItem(R.id.menu_item_share);
+        }
+        return super.onCreateOptionsMenu(menu);
 
     }
 
@@ -61,5 +77,28 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
     public void recreateLoader() {
         MainActivityFragment maf = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.main_fragment);
         maf.getLoaderManager().restartLoader(MainActivityFragment.LOADER_ID, null, maf);
+    }
+
+    @Override
+    public void recreateShareProvider() {
+        DetailActivityFragment daf = (DetailActivityFragment)
+                getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
+
+        daf.mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if (daf.mUri != null) {
+            Intent i = daf.createShareMovieIntent();
+            if (i == null)
+                return;
+            daf.mShareActionProvider.setShareIntent(i);
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null or no trailers");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        MainActivityFragment maf = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.main_fragment);
+        maf.getLoaderManager().destroyLoader(maf.LOADER_ID);
+        super.onDestroy();
     }
 }
